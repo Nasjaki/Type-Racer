688abd6e120697a4a41694554b2ab9dc204c0c38
Nasjaki <109064535+Nasjaki@users.noreply.github.com>
2022-07-12 12:10:59 +0200

    Add files via upload

---
+++ react-app2/src/Profile.js
@@ -0,0 +1,22 @@
+
+
+
+function Profile() {
+
+    return ( 
+        <div className='ProfileClass'>
+
+            <h1>
+                Profile
+            </h1>
+
+            
+            
+        </div>
+
+    );
+    
+
+}
+
+export default Profile;
